84f3f553ffb96a844c1f7c1f6e89fec410918c82
Darren Leck <jiemingdarren49@gmail.com>
2015-08-18 09:53:24 +0800

    Added sound library to be implemented later on

--- Others/Source/Sound.cpp
+++ Others/Source/Sound.cpp
@@ -1,4 +1,5 @@
 #include "Sound.h"
+ISoundEngine * engine;
 
 Sound::Sound()
 {
@@ -7,3 +8,34 @@ Sound::Sound()
 Sound::~Sound()
 {
 }
+
+//int Sound::soundInit()
+//{
+//	engine = createIrrKlangDevice();
+//	if(!engine)
+//		return 0;
+//	return 0;
+//}
+
+/*
+ISoundEngine * engine = createIrrKlangDevice();
+if(!engine)
+*/
+
+/*
+engine->play2D("../../media/getout.ogg", true);
+
+extern ISoundEngine * engine; add this to respective classes
+
+eg. while(i != 'q')
+{
+engine->play2D("../../media/...");
+}
+*/
+
+/*
+put code in exit
+engine->stopAllSounds();
+
+*/
+
--- Others/Source/Sound.h
+++ Others/Source/Sound.h
@@ -1,8 +1,14 @@
 #pragma once
+#include <irrKlang.h>
 
+using namespace irrklang;
+
+#pragma comment(lib,"irrKlang.lib")
 class Sound
 {
 public:
 	Sound();
 	~Sound();
+
+	//int soundInit();
 };
