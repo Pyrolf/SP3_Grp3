744faa1c3dc38c6298efc082337aea934776d6ce
shingliya <damianivan@hotmail.com>
2015-08-19 10:14:41 +0800

    Revert the collision

--- GameObjects/Source/GameObjectFactory.cpp
+++ GameObjects/Source/GameObjectFactory.cpp
@@ -39,38 +39,20 @@ void GameObjectFactory::generateGO(CMap *map)
 
 GameObject* GameObjectFactory::CheckColision(Vector3 pos)
 {
-	Vector3 temp1, temp2;
-	float distSquared;
+	Vector3 min(pos.x + 1, pos.y + 1, 0);
+	Vector3 max(pos.x + tileSize - 1, pos.y + tileSize - 1, 0);
 
 	for(int i = 0; i < GoList.size(); ++i)
-	{
-		temp1.Set(pos.x, 0, 0);
-		temp2.Set(GoList[i]->pos.x, 0, 0);
-		distSquared = (temp1 - temp2).LengthSquared();
-		
-		if(distSquared < tileSize * tileSize)
-		{
-			temp1.Set(0, pos.y, 0);
-			temp2.Set(0, GoList[i]->pos.y, 0);
-			distSquared = (temp1 - temp2).LengthSquared();
-
-			if(distSquared < tileSize * tileSize)
-			{
-				return GoList[i];
-			}
-		}
-	}
-	return NULL;
-
-	/*Vector3 min(pos.x + 1, pos.y + 1, 0);
-	Vector3 max(pos.x + tileSize - 1, pos.y + tileSize - 1, 0);
-		
+	{	
 		
 	if((min.x > GoList[i]->pos.x && min.x < GoList[i]->pos.x + tileSize) && (min.y > GoList[i]->pos.y && min.y < GoList[i]->pos.y + tileSize) || 
 		(min.x > GoList[i]->pos.x && min.x < GoList[i]->pos.x + tileSize) && (max.y > GoList[i]->pos.y && max.y < GoList[i]->pos.y + tileSize) ||
 		(max.x > GoList[i]->pos.x && max.x < GoList[i]->pos.x + tileSize) && (min.y > GoList[i]->pos.y && min.y < GoList[i]->pos.y + tileSize) ||			(max.x > GoList[i]->pos.x && max.x < GoList[i]->pos.x + tileSize) && (max.y > GoList[i]->pos.y && max.y < GoList[i]->pos.y + tileSize))
 	{
 		return GoList[i];
-	}*/	
+	}
+		
+	}
+	return NULL;
 }
 
